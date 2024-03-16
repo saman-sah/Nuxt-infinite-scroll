a84df3c9e75470d265efe065ee1e9f5a1792801d
saman-sah <sahraei.saman91@gmail.com>
2024-03-16 18:48:08 +0330

    init nuxt

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 class="h1 text-red-600">Home page</h1>
+</template>
+
+<script setup>
+const limit = 10;
+const skip = 0;
+const data = await $fetch('/api/user?limit='+limit+'&skip='+skip )
+console.log(data.users);
+
+// const element = document.documentElement
+// const clientHeight = element.clientHeight
+// const scrollHeight = element.scrollHeight
+// const scrollTop = element.scrollTop
+
+// if(scrollTop + clientHeight >= scrollHeight) {
+//   // Call Api for more data
+// }
+</script>
---
+++ server/api/user.js
@@ -0,0 +1,29 @@
+import users from '../db/users.json'
+
+export default defineEventHandler(async (event) => {
+  const query = getQuery(event)
+  console.log('query', query);
+  const { limit, skip } = query
+  const getUsers = async (limit = 1, skip = 1) => {
+    try {
+      const response = await $fetch('https://testapi.devtoolsdaily.com/users?limit' + limit + '' + skip);
+
+      return response;
+    } catch (error) {
+      console.error('Error fetching users:', error);
+      throw error;
+    }
+  }
+
+
+
+
+
+  const users = await getUsers();
+  return {
+    users
+  }
+  return {
+    users
+  }
+});
